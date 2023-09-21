2b67e12890977e7e11cb27b458bd7bd31c989a83
berkingurcan <berkingurcan@yandex.com>
2023-09-21 12:16:03 +0300

    optimized code from gpt

--- discord-getter/src/main.rs
+++ discord-getter/src/main.rs
@@ -33,58 +33,53 @@ impl EventHandler for Handler {
         let _messages = channel_id
             .messages(&ctx, |retriever| retriever.after(MessageId(1153348746881540206)).limit(100))
             .await;
-
-        let extracted_data: Result<Vec<_>, _> = _messages.map(|messages| {
-            messages.iter().map(|message| {
-                (
+    
+        let mut writer = Writer::from_path("output.csv").unwrap();
+    
+        if let Ok(messages) = _messages {
+            for message in messages {
+                let data = (
                     message.id,
                     message.channel_id,
                     message.author.name.clone(),
                     message.content.clone(),
                     message.timestamp,
-                    message.mentions.iter().map(|user| (user.id, user.name.clone())).collect::<Vec<_>>(),
-                    message.reactions.iter().map(|reaction| (reaction.count, reaction.reaction_type.clone())).collect::<Vec<_>>(),
-                    message.referenced_message.as_ref().map(|referenced_message| {
-                        (referenced_message.id, referenced_message.content.clone())
-                    }),
+                    message
+                        .mentions
+                        .iter()
+                        .map(|user| format!("{}: {}", user.id, user.name))
+                        .collect::<Vec<_>>()
+                        .join(","),
+                    message
+                        .reactions
+                        .iter()
+                        .map(|reaction| format!("{}, {}", reaction.count, reaction.reaction_type))
+                        .collect::<Vec<_>>()
+                        .join(","),
+                    message
+                        .referenced_message
+                        .as_ref()
+                        .map(|referenced_message| format!("{}: {}", referenced_message.id, referenced_message.content)),
                     message.member.as_ref().map(|memb| {
-                        (memb.nick.as_ref().map(String::to_string), memb.roles.iter().map(|x| x.to_string() + ",").collect::<String>())
+                        (
+                            memb.nick.as_ref().map_or("NONE", |n| &n),
+                            memb.roles.iter().map(|x| x.to_string() + ",").collect::<String>(),
+                        )
                     }),
-                )
-            }).collect()
-        });
-
-        let mut unwrapped_extracted_data = extracted_data.unwrap();
-        let mut writer = Writer::from_path("output.csv").unwrap();
-
-        for data in unwrapped_extracted_data {
-            writer.write_record(&[
-                data.0.to_string(),
-                data.1.to_string(),
-                data.2.to_string(),
-                data.3.to_string(),
-                data.4.to_string(),
-                data.5.iter()
-                    .map(|(id, name)| format!("{}: {}", id, name))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-                data.6.iter()
-                    .map(|(count, reaction_type)| format!("{}, {}", count, reaction_type))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-                data.7.iter()
-                    .map(|(id, content)| format!("{}: {}", id, content))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-                data.8.iter()
-                    .map(|(nick, roles)| format!("{}: {}", nick.as_ref().map_or("NONE", |n| n), roles))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-            ]);
+                );
+    
+                writer.write_record(&[
+                    data.0.to_string(),
+                    data.1.to_string(),
+                    data.2.to_string(),
+                    data.3.to_string(),
+                    data.4.to_string(),
+                    data.5,
+                    data.6,
+                    data.7.unwrap_or_default(), // referenced_message
+                    data.8.map_or("NONE,NONE".to_string(), |(nick, roles)| format!("{}: {}", nick, roles)),
+                ]);
+            }
         }
     }
 

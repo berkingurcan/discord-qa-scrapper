f6de2aa0644bbb0a00eb99eadf62d51553a3c82e
berkingurcan <berkingurcan@yandex.com>
2023-09-25 13:27:04 +0300

    Update main.rs

--- discord-getter/src/main.rs
+++ discord-getter/src/main.rs
@@ -29,63 +29,20 @@ impl EventHandler for Handler {
     // events can be dispatched simultaneously.
 
     async fn message(&self, ctx: Context, _msg: Message) {
-        let channel_id = ChannelId(1153348653122076676);
-        let _messages = channel_id
-            .messages(&ctx, |retriever| retriever.after(MessageId(1153348746881540206)).limit(100))
-            .await;
+        let channel_id = ChannelId(1154341442706231387);
 
-        let extracted_data: Result<Vec<_>, _> = _messages.map(|messages| {
-            messages.iter().map(|message| {
-                (
-                    message.id,
-                    message.channel_id,
-                    message.author.name.clone(),
-                    message.content.clone(),
-                    message.timestamp,
-                    message.mentions.iter().map(|user| (user.id, user.name.clone())).collect::<Vec<_>>(),
-                    message.reactions.iter().map(|reaction| (reaction.count, reaction.reaction_type.clone())).collect::<Vec<_>>(),
-                    message.referenced_message.as_ref().map(|referenced_message| {
-                        (referenced_message.id, referenced_message.content.clone())
-                    }),
-                    message.member.as_ref().map(|memb| {
-                        (memb.nick.as_ref().map(String::to_string), memb.roles.iter().map(|x| x.to_string() + ",").collect::<String>())
-                    }),
-                )
-            }).collect()
-        });
+        let _archived_threads = channel_id.get_archived_public_threads(ctx, None, None);
+        // let _active_threads = channel_id.get_active_threads();
 
-        let mut unwrapped_extracted_data = extracted_data.unwrap();
-        let mut writer = Writer::from_path("output.csv").unwrap();
-
-        for data in unwrapped_extracted_data {
-            writer.write_record(&[
-                data.0.to_string(),
-                data.1.to_string(),
-                data.2.to_string(),
-                data.3.to_string(),
-                data.4.to_string(),
-                data.5.iter()
-                    .map(|(id, name)| format!("{}: {}", id, name))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-                data.6.iter()
-                    .map(|(count, reaction_type)| format!("{}, {}", count, reaction_type))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-                data.7.iter()
-                    .map(|(id, content)| format!("{}: {}", id, content))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-                data.8.iter()
-                    .map(|(nick, roles)| format!("{}: {}", nick.as_ref().map_or("NONE", |n| n), roles))
-                    .collect::<Vec<_>>()
-                    .join(",")
-                    .to_string(),
-            ]);
+        match (_archived_threads).await {
+            Ok(data) => println!("{:?}", data),
+            Err(error) => println!("{:?}", error),
         }
+
+
+        
+
+        
     }
 
     // Set a handler to be called on the `ready` event. This is called when a

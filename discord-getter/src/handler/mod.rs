7fe0ac8cbda91269e3762034c0a63cee2d3f85c8
berkingurcan <berkingurcan@yandex.com>
2023-09-27 13:26:57 +0300

    mod ready

--- discord-getter/src/handler/mod.rs
+++ discord-getter/src/handler/mod.rs
@@ -5,6 +5,12 @@ use serenity::model::gateway::Ready;
 use serenity::model::prelude::GuildId;
 use serenity::model::id::{ChannelId, MessageId};
 
+pub mod ready;
+
+use crate::handler::{
+    ready::handle_ready,
+};
+
 use csv::Writer;
 
 pub struct Handler;
@@ -311,13 +317,8 @@ impl EventHandler for Handler {
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        handle_ready(ctx, ready).await;
     }
+
 }
--- discord-getter/src/handler/ready.rs
+++ discord-getter/src/handler/ready.rs
@@ -1,6 +1,12 @@
 use serenity::prelude::*;
 use serenity::model::gateway::Ready;
 
+// Set a handler to be called on the `ready` event. This is called when a
+// shard is booted, and a READY payload is sent by Discord. This payload
+// contains data like the current user's guild Ids, current user data,
+// private channels, and more.
+//
+// In this case, just print what the current user's username is.
 pub async fn handle_ready(_: Context, ready: Ready) {
     println!("{} is connected!", ready.user.name);
 }

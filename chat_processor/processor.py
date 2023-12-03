06b2acedfe8afd43d2dc00bd59c66ade53ce9162
berkingurcan <berkingurcan@yandex.com>
2023-12-03 14:14:45 +0700

    Update processor.py

---
+++ chat_processor/processor.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+pair1 = pd.read_csv('assistant_processed_chat/chat1.csv')
+pair2 = pd.read_csv('assistant_processed_chat/chat2.csv')
+pair3 = pd.read_csv('assistant_processed_chat/chat3.csv')
+
+pair2.columns = pair1.columns
+pair3.columns = pair1.columns
+
+df = pd.concat([pair1, pair2, pair3], ignore_index=True)
+print(len(df))

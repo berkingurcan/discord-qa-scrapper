89d56bf84abe6d0d44a01c973e0098bea3454abf
berkingurcan <berkingurcan@yandex.com>
2023-12-03 15:44:49 +0700

    feat add export json

--- chat_processor/processor.py
+++ chat_processor/processor.py
@@ -10,4 +10,19 @@ pair2.columns = pair1.columns
 pair3.columns = pair1.columns
 
 df = pd.concat([pair1, pair2, pair3], ignore_index=True)
-print(len(df))
+
+os.mkdir('assistant_processed_chat/results')
+
+for index, row in df.iterrows():
+    question = row['question']
+    answer = row['answer']
+
+    file_path = 'assistant_processed_chat/results/' + str(index) + '.json'
+    json_data = json.dumps({
+            "question": question,
+            "answer": answer
+        }, indent=2)
+    
+    with open(file_path, 'w') as file:
+        file.write(json_data)
+    

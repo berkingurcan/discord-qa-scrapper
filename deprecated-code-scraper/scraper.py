4136a2caa233c6c4614f8050e09c1fcf9e23b3b6
berkingurcan <berkingurcan@yandex.com>
2023-12-27 12:52:29 +0300

    gets com, code

--- deprecated-code-scraper/scraper.py
+++ deprecated-code-scraper/scraper.py
@@ -2,10 +2,34 @@ import re
 import json
 import os
 
+def extract_comments_and_code_elements(ts_code):
+    # Regex pattern for comments in the /** ... */ format and the following code element
+    pattern = r'(/\*\*[\s\S]*?\*/)\s*([^\s].*?)\s*[{;]'
+
+    # Find all matches
+    matches = re.findall(pattern, ts_code)
+
+    # Process matches to format them
+    extracted = []
+    for comment, code_element in matches:
+        extracted.append({"Comment": comment.strip(), "Code Element": code_element.strip()})
+
+    return extracted
+
 def find_deprecated_code(directory):
     deprecated_code = []
     
-    
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".ts"):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    content = f.read()
+                    extracted_content = extract_comments_and_code_elements(content)
+                    
+                    for f in extracted_content:
+                        if 'deprecated' in f['Comment']:
+                            deprecated_code.append(f)
 
     return deprecated_code
 
